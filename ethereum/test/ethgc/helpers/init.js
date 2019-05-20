8e420f12af9cdd35dc76f2bfb62672c83a4d9454
Jose <netinho.workspace@gmail.com>
2019-05-20 16:13:41 -0700

    Re-enable tests (#40)
    * Re-enable tests
    
    * Fix eth tests

--- ethereum/test/ethgc/helpers/init.js
+++ ethereum/test/ethgc/helpers/init.js
@@ -2,8 +2,8 @@ const deploy = require("../../../../library/src/deploy");
 const EthGc = require("../../../../library/src/ethgc/ethGcNetwork");
 
 module.exports = async function init(accounts) {
-  await deploy.deploy(accounts[0], [web3.currentProvider]);
-  const ethGc = new EthGc(web3.currentProvider);
+  await deploy.deploy(true, accounts[0], [web3.currentProvider]);
+  const ethGc = new EthGc(true, web3.currentProvider);
   ethGc.hardlyWeb3.switchAccount(accounts[0]);
   return ethGc;
 };
